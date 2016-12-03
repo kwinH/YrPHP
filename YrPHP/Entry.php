2f08e5c48ec536cc1df844b74e7c281d13d4f06a
kwinwong <kwinwong@hotmail.com>
2016-12-03 09:31:55 +0800

    完善composer自动加载机制

--- YrPHP/Entry.php
+++ YrPHP/Entry.php
@@ -5,7 +5,7 @@
  * User: Nathan
  * QQ:284843370
  * Email:kwinwong@hotmail.com
- * GitHub:https://GitHubhub.com/kwinH/YrPHP
+ * GitHub:https://github.com/kwinH/YrPHP
  */
 use YrPHP\Debug;
 use YrPHP\File;

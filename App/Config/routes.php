dd131f46b41e0b5cc1cc73445a6b91881c6449a5
kwin wong <kwinwong@hotmail.com>
2021-03-17 15:31:22 +0800

    delete test code

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -7,33 +7,4 @@
  * GitHub:https://github.com/kwinH/YrPHP
  */
 
-//Route::get('/',  'Test\Index@index');
-
-//    Route::controller('user', 'UserController');
-Route::group([
-    'namespace' => 'App\\Controllers',
-    // 'view'=>'test'
-], function ($route) {
-    $route->get('/',function(){
-       return 'ddd';
-    });
-    $route->controller('user', 'UserController');
-
-
-
-
-    Route::group([
-        'namespace' => '\\Test'
-    ], function ($route) {
-        $route->controller('test', 'Index');
-    });
-
-});
-
-
-//Route::group([
-//   'namespace' =>'App\\Controllers\\Test\\'
-//],function($route){
-//    $route->controller('test', 'Index');
-//});
-
+Route::get('/',  'Index@index');
